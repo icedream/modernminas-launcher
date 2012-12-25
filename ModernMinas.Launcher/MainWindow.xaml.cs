2a5a4b8fd2ab5476a6cdbf2bc4f68e7cfd4fc38a
Icedream <icedream@modernminas.tk>
2012-12-25 23:17:39 +0100

    Adding GUI code (not functional yet).

---
+++ ModernMinas.Launcher/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernMinas.Launcher
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SetStatus()
+        {
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.LoginPanel.Visibility = System.Windows.Visibility.Visible;
+            this.BottomContentPanel.Height = 60;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
