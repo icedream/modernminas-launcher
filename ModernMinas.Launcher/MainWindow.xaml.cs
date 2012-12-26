446aa9261ebec840ad4b6b8c5c0e8d913ef9e24b
Icedream <icedream@modernminas.tk>
2012-12-26 01:49:34 +0100

    Animations! - Added: LoginPanel/ProgressPanel fading

--- ModernMinas.Launcher/MainWindow.xaml.cs
+++ ModernMinas.Launcher/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -30,13 +31,60 @@ namespace ModernMinas.Launcher
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            this.LoginPanel.Visibility = System.Windows.Visibility.Visible;
             this.BottomContentPanel.Height = 60;
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
+            Fade(LoginPanel, 0, null, 250, (a, b) => {
+                LoginPanel.Visibility = System.Windows.Visibility.Collapsed;
+                ProgressPanel.Visibility = System.Windows.Visibility.Visible;
+                ProgressPanel.Opacity = 0;
+                Fade(ProgressPanel, 1, null, 250);
+            });
+        }
 
+        public void Fade(FrameworkElement c, double targetOpacity, EasingFunctionBase f = null, double ms = 500.0, EventHandler onFinish = null)
+        {
+            Storyboard storyboard = new Storyboard();
+            TimeSpan duration = TimeSpan.FromMilliseconds(ms);
+
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = c.Opacity;
+            animation.To = targetOpacity;
+            animation.Duration = new Duration(duration);
+
+            if ((animation.EasingFunction = f) == null)
+            {
+                var easing = new SineEase();
+                easing.EasingMode = EasingMode.EaseOut;
+                animation.EasingFunction = easing;
+            }
+
+            Storyboard.SetTargetName(animation, c.Name);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(Control.OpacityProperty));
+
+            storyboard.Children.Add(animation);
+
+            if (onFinish != null)
+                storyboard.Completed += onFinish;
+
+            storyboard.Begin(this);
+        }
+
+        private void Main_Initialized(object sender, EventArgs e)
+        {
+        }
+
+        private void Main_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        private void Main_ContentRendered(object sender, EventArgs e)
+        {
+            LoginPanel.Visibility = System.Windows.Visibility.Visible;
+            LoginPanel.Opacity = 0;
+            Fade(LoginPanel, 1, null, 1000.0);
         }
     }
 }
