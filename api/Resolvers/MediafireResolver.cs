9376a2de9f51b57774cdc110177e75ed78b5269d
icedream <icedream2k9@die-optimisten.net>
2013-03-06 19:46:16 +0100

    MediafireResolver: Do not use non-closing streams anymore.

--- api/Resolvers/MediafireResolver.cs
+++ api/Resolvers/MediafireResolver.cs
@@ -21,7 +21,7 @@ namespace ModernMinas.Update.Api.Resolvers
             var originUrl = new Uri(string.Format("http://mediafire.com/?{0}", Expand(resolverNode.SelectSingleNode("child::id").InnerText)));
             var url = originUrl;
             Uri oldM = null;
-            
+
             MemoryStream ms = new MemoryStream();
 
         retry1:
@@ -33,10 +33,9 @@ namespace ModernMinas.Update.Api.Resolvers
             // We are going to do a manual request with manual parsing here. ._.
             var socket1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             socket1.Connect(new DnsEndPoint(url.Host, 80));
-            Stream s = new BufferedStream(new NetworkStream(socket1));
-            using (var ncs = new NonClosingStream(s))
-            using (var sr = new StreamReader(ncs))
-            using (var sw = new StreamWriter(ncs))
+            var s = new BufferedStream(new NetworkStream(socket1));
+            using (var sr = new StreamReader(s))
+            using (var sw = new StreamWriter(s))
             {
                 sw.WriteLine("GET {0} HTTP/1.0", url.PathAndQuery);
                 sw.WriteLine("Host: {0}", url.Host);
@@ -59,24 +58,23 @@ namespace ModernMinas.Update.Api.Resolvers
                     Log.InfoFormat("Server returned header: {0} = {1}", n, v);
                     headers.Add(n, v);
                 }
-            }
-            oldM = url;
-            if (headers.ContainsKey("location"))
-            {
-                Log.InfoFormat("Server redirected us to {0}", headers["location"]);
-                url = new Uri(url, headers["location"]);
-                goto retry1;
-            }
-            
-            long length;
-            byte[] buffer;
 
-            if (headers.ContainsKey("content-type") && headers["content-type"].Contains("text/html"))
-            {
-                Log.Info("We were served an HTML page, resolving it to the actual download link");
-                Uri m;
-                using (StreamReader sr = new StreamReader(s))
+                oldM = url;
+                if (headers.ContainsKey("location"))
                 {
+                    Log.InfoFormat("Server redirected us to {0}", headers["location"]);
+                    url = new Uri(url, headers["location"]);
+                    goto retry1;
+                }
+
+                long length;
+                byte[] buffer;
+
+                if (headers.ContainsKey("content-type") && headers["content-type"].Contains("text/html"))
+                {
+                    Log.Info("We were served an HTML page, resolving it to the actual download link");
+                    Uri m;
+
                     var c = sr.ReadToEnd();
 
                     // download_repair.php, fixes empty URI exception.
@@ -91,71 +89,67 @@ namespace ModernMinas.Update.Api.Resolvers
                         m = new Uri(Regex.Match(c, "kNO = \"(.+)\"").Groups[1].Value);
                         Log.InfoFormat("Resolved to: {0}", m.ToString());
                     }
+
+                    url = m;
+                    goto retry1;
                 }
-                s.Close();
-                s.Dispose();
-
-                url = m;
-                goto retry1;
-            }
-            else
-            {
-                Log.Info("We were served a non-HTML file, downloading");
-            }
-
-            if (!headers.ContainsKey("content-length"))
-            {
-                Log.Error("Content-length header missing.");
-                throw new Exception("Mediafire server did not send back content-length header. Please contact the developer.");
-            }
-
-            length = long.Parse(headers["content-length"]);
-            Log.InfoFormat("Content-length is {0}.", length);
-
-            buffer = new byte[2048];
-            Task.Factory.StartNew(() =>
-            {
-                var l0 = 0;
-                var l = 1;
-                while (l0 < 3) // ignore length for debugging purposes
+                else
                 {
-                    l = s.Read(buffer, 0, buffer.Length);
-                    if (l == 0)
-                    {
-                        l0++;
-                        continue;
-                    }
-                    else
-                    {
-                        l0 = 0;
-                        ms.Write(buffer, 0, l);
-                    }
+                    Log.Info("We were served a non-HTML file, downloading");
                 }
-                
-                length = ms.Position;
-            });
-            Task.Factory.StartNew(() =>
-            {
+
+                if (!headers.ContainsKey("content-length"))
+                {
+                    Log.Error("Content-length header missing.");
+                    throw new Exception("Mediafire server did not send back content-length header. Please contact the developer.");
+                }
+
+                length = long.Parse(headers["content-length"]);
+                Log.InfoFormat("Content-length is {0}.", length);
+
+                buffer = new byte[4096];
+                Task.Factory.StartNew(() =>
+                {
+                    var l0 = 0;
+                    var l = 1;
+                    while (l0 < 3) // ignore length for debugging purposes
+                    {
+                        l = sr.BaseStream.Read(buffer, 0, buffer.Length);
+                        if (l == 0)
+                        {
+                            l0++;
+                            continue;
+                        }
+                        else
+                        {
+                            l0 = 0;
+                            ms.Write(buffer, 0, l);
+                        }
+                    }
+
+                    length = ms.Position;
+                });
+                Task.Factory.StartNew(() =>
+                {
+                    while (ms.Position < length)
+                    {
+                        OnStatusChanged(ms.Position / length, StatusType.Downloading);
+                        System.Threading.Thread.Sleep(50);
+                    }
+                });
                 while (ms.Position < length)
                 {
-                    OnStatusChanged(ms.Position / length, StatusType.Downloading);
                     System.Threading.Thread.Sleep(50);
                 }
-            });
-            while (ms.Position < length)
-            {
-                System.Threading.Thread.Sleep(50);
+
+                OnStatusChanged(1, StatusType.Downloading);
+                Log.InfoFormat("Download finished (received {0} bytes).", ms.Position);
+                ms.Flush();
+                ms.Seek(0, SeekOrigin.Begin);
+                Log.Info("File saved.");
+
+                return ms;
             }
-
-            OnStatusChanged(1, StatusType.Downloading);
-            Log.InfoFormat("Download finished (received {0} bytes).", ms.Position);
-            ms.Flush();
-            ms.Seek(0, SeekOrigin.Begin);
-            s.Close();
-            s.Dispose();
-            Log.Info("File saved.");
-
-            return ms;
         }
     }
 }
