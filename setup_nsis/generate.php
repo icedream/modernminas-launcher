45a77b93c7a7e73d90acf746d66edb565cc3ffb1
icedream <icedream2k9@die-optimisten.net>
2013-03-16 15:21:06 +0100

    More flexibility for setup_nsis/generate.php!

--- setup_nsis/generate.php
+++ setup_nsis/generate.php
@@ -4,7 +4,7 @@ $version = str_replace(".", "_", (string) $assembly -> assemblyIdentity["version
 $path = "Application Files/mmlaunch_$version";
 
 if(!file_exists("$path/setup.exe"))
-  system(escapeshellarg("C:\\Program Files (x86)\\NSIS\\MakeNSIS.exe") . " /DCONFIGURATION=Release /DSOURCE_BIN_PATH=../launcher/bin/Release /DVERSION=$version setup\\Setup.nsi");
+  system(escapeshellarg("C:\\Program Files (x86)\\NSIS\\MakeNSIS.exe") . " /DCONFIGURATION=Release /DSOURCE_BIN_PATH=../launcher/bin/Release /DVERSION=$version ".escapeshellarg(dirname(__FILE__) . "\\Setup.nsi"));
 if(!file_exists("$path/setup.exe"))
   die(header("HTTP/1.1 404 Not Found"));
 $path = str_replace(" ", "%20", $path);
